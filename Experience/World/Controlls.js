648688f8dd6423de04f78ebb029ca047bff8d87c
Paras Arya <parasarya289@gmail.com>
2023-12-08 23:15:34 +0530

    removed assist curve

--- Experience/World/Controlls.js
+++ Experience/World/Controlls.js
@@ -1,3 +1,4 @@
+import GSAP from "gsap";
 import * as THREE from "three";
 import Experience from "../Experience";
 
@@ -7,30 +8,12 @@ export default class Controlls {
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
     this.time = this.experience.time;
-
-    this.setPath();
-  }
-  setPath() {
-    this.curve = new THREE.CatmullRomCurve3(
-      [
-        new THREE.Vector3(-10, 0, 10),
-        new THREE.Vector3(-5, 5, 5),
-        new THREE.Vector3(0, 0, 0),
-        new THREE.Vector3(5, -5, 5),
-        new THREE.Vector3(10, 0, 10),
-      ],
-      true
-    );
-
-    const points = this.curve.getPoints(50);
-    const geometry = new THREE.BufferGeometry().setFromPoints(points);
-
-    const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
-
-    // Create the final object to add to the scene
-    const curveObject = new THREE.Line(geometry, material);
-    this.scene.add(curveObject);
+    this.camera = this.experience.camera;
+    this.progress = 0;
+    this.dummyVector = new THREE.Vector3(0, 0, 0);
+    this.position = new THREE.Vector3(0, 0, 0);
   }
   resize() {}
-  update() {}
+  update() {
+  }
 }
