2d2faa019699c2d1bd82a03b161af2e7d401fd1e
Paras Arya <parasarya289@gmail.com>
2023-12-11 00:00:24 +0530

    fix: floor rotation

--- Experience/World/Floor.js
+++ Experience/World/Floor.js
@@ -12,8 +12,10 @@ export default class Floor {
     this.geometry = new THREE.PlaneGeometry(100, 100);
     this.material = new THREE.MeshStandardMaterial({
       color: 0xffffff,
+      side: THREE.BackSide,
     });
     this.plane = new THREE.Mesh(this.geometry, this.material);
+    this.plane.rotation.x = Math.PI / 2;
     this.scene.add(this.plane);
   }
   resize() {}
