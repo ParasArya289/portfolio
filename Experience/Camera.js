64b111c4f7e9d1e266f76e1d58b62b18d5a6041f
Paras Arya <parasarya289@gmail.com>
2023-12-12 03:10:43 +0530

    changed orthographic camera position

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -38,8 +38,8 @@ export default class Camera {
         );
 
         // 6.5
-        this.orthographicCamera.position.y = 5.65;
-        this.orthographicCamera.position.z = 10;
+        this.orthographicCamera.position.y = 3.5;
+        this.orthographicCamera.position.z = 5;
         this.orthographicCamera.rotation.x = -Math.PI / 6;
 
         this.scene.add(this.orthographicCamera);
