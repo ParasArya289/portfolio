89514fad869eec2198ad3a69b8a9dbbd3e2c70dc
Paras Arya <parasarya289@gmail.com>
2023-12-12 03:11:20 +0530

    gsap ScrollTrigger to move model

--- Experience/World/Controlls.js
+++ Experience/World/Controlls.js
@@ -1,14 +1,33 @@
-import GSAP from "gsap";
-import * as THREE from "three";
 import Experience from "../Experience";
+import GSAP from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import * as THREE from "three";
 
 export default class Controlls {
   constructor() {
     this.experience = new Experience();
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
+    this.sizes = this.experience.sizes;
     this.time = this.experience.time;
     this.camera = this.experience.camera;
+    this.room  = this.experience.world.room.actualRoom;
+    GSAP.registerPlugin(ScrollTrigger);
+    this.setPath();
+  }
+  setPath(){
+    this.timeline = new GSAP.timeline();
+    this.timeline.to(this.room.position,{
+      x:()=>this.sizes.width * 0.0015,
+      scrollTrigger:{
+        trigger:".first-margin",
+        markers:true,
+        start:"top top",
+        end:"bottom bottom",
+        scrub:0.6,
+        invalidateOnRefresh:true
+      }
+    })
   }
   resize() {}
   update() {
