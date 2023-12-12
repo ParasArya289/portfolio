ced80c7e306b3564436c2bf165103f1f72eda4e8
Paras Arya <parasarya289@gmail.com>
2023-12-12 23:53:34 +0530

    feat: change theme as per user device preference

--- Experience/Theme.js
+++ Experience/Theme.js
@@ -1,11 +1,44 @@
 import { EventEmitter } from "events";
+import Experience from "./Experience";
+import Resources from "./Utils/Resources";
 export default class Theme extends EventEmitter {
   constructor() {
     super();
+    this.experience = new Experience();
+    this.resources = this.experience.resources;
     this.theme = "light";
     this.toggleButton = document.querySelector(".toggle-btn");
     this.toggleCircle = document.querySelector(".toggle-circle");
     this.setEventListeners();
+
+    this.resources.on("ready", () => {
+      setTimeout(() => {
+        const prefersDarkMode = window.matchMedia(
+          "(prefers-color-scheme: dark)"
+        ).matches;
+        if (prefersDarkMode) {
+          this.toggleCircle.classList.toggle("slide");
+          this.theme = "dark";
+          this.emit("switch", this.theme);
+        }
+        console.log("event emitted", this.theme);
+      }, 0);
+    });
+
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (e) => {
+        const isDarkMode = e.matches;
+        if (isDarkMode) {
+          this.toggleCircle.classList.add("slide");
+          this.theme = "dark";
+          this.emit("switch", this.theme);
+        } else {
+          this.toggleCircle.classList.remove("slide");
+          this.theme = "light";
+          this.emit("switch", this.theme);
+        }
+      });
   }
   setEventListeners() {
     this.toggleButton.addEventListener("click", () => {
