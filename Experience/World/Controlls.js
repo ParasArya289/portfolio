31b76ba521b748955fd4d18d63cbbf6e02178423
Paras Arya <parasarya289@gmail.com>
2024-01-08 01:20:01 +0530

    fixed: gsap not reseting respective display timelines

--- Experience/World/Controlls.js
+++ Experience/World/Controlls.js
@@ -38,8 +38,13 @@ export default class Controlls {
 
     GSAP.registerPlugin(ScrollTrigger);
     document.querySelector(".page").style.overflow = "visible";
-
-    this.setSmoothScroll();
+    if (
+      !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      this.setSmoothScroll();
+    }
     this.setScrollTrigger();
     this.setProjectScroll();
   }
@@ -63,16 +68,14 @@ export default class Controlls {
     };
     const scroll = (e) => {
       const { value } = e.target;
-      const sectionWrapper = document.querySelector(".project-detail-wrapper")
-      const sections =
-      sectionWrapper.querySelectorAll(".projects-section");
+      const sectionWrapper = document.querySelector(".project-detail-wrapper");
+      const sections = sectionWrapper.querySelectorAll(".projects-section");
 
       sections.forEach((section) => {
         //scroll
         if (section.classList.contains(value)) {
-          const scrollLeft =
-            section.offsetLeft - sectionWrapper.offsetLeft;
-            sectionWrapper.scrollTo({
+          const scrollLeft = section.offsetLeft - sectionWrapper.offsetLeft;
+          sectionWrapper.scrollTo({
             left: scrollLeft,
             behavior: "smooth",
           });
@@ -133,8 +136,13 @@ export default class Controlls {
 
   //TO BE FIXED: Glitch on mobile view
   //matchmedia is working but gsap is freaking out on scale
+  //FIXED: version 3.10 to use scrollTrigger matchmedia fixes issue
+  
   setScrollTrigger() {
     // const scroll = GSAP.matchMedia();
+    // scroll.add("(min-width:969px)", () => {});
+    // scroll.add("(max-width:968px)", () => {});
+
     ScrollTrigger.matchMedia({
       "(min-width:969px)": () => {
         console.log("desktop");
@@ -208,7 +216,9 @@ export default class Controlls {
           .to(
             this.room.position,
             {
-              x: 1,
+              x: () => {
+                return 1;
+              },
               z: () => {
                 return this.sizes.height * 0.0032;
               },
@@ -235,8 +245,8 @@ export default class Controlls {
           .to(
             this.aquariamRectLight,
             {
-              height: 0.7 * 4,
               width: 0.5 * 4,
+              height: 0.7 * 4,
             },
             "same"
           )
@@ -293,32 +303,27 @@ export default class Controlls {
             scrub: 0.6,
             // invalidateOnRefresh: true,
           },
-        })
-          .to(
-            this.room.scale,
-            {
-              x: 0.1,
-              y: 0.1,
-              z: 0.1,
-            },
-            "same"
-          )
-          .to(
-            this.aquariamRectLight,
-            {
-              width: this.aquariamRectLight.width * 1.4,
-              height: this.aquariamRectLight.height * 1.4,
-            },
-            "same"
-          )
-          .to(
-            this.monitorRectLight,
-            {
-              width: this.monitorRectLight.width * 1.4,
-              height: this.monitorRectLight.height * 1.4,
-            },
-            "same"
-          );
+        }).to(this.room.scale, {
+          x: 0.1,
+          y: 0.1,
+          z: 0.1,
+        });
+        // .to(
+        //   this.aquariamRectLight,
+        //   {
+        //     width: this.aquariamRectLight.width * 1.4,
+        //     height: this.aquariamRectLight.height * 1.4,
+        //   },
+        //   "same"
+        // )
+        // .to(
+        //   this.monitorRectLight,
+        //   {
+        //     width: this.monitorRectLight.width * 1.4,
+        //     height: this.monitorRectLight.height * 1.4,
+        //   },
+        //   "same"
+        // );
 
         //second
         this.secondMoveTimeline = new GSAP.timeline({
@@ -342,24 +347,24 @@ export default class Controlls {
           .to(
             this.aquariamRectLight,
             {
-              width: this.aquariamRectLight.width * 3.4,
-              height: this.aquariamRectLight.height * 3.4,
+              width: 0.35 * 3.4,
+              height: 0.49 * 3.4,
             },
             "same"
           )
           .to(
             this.monitorRectLight,
             {
-              width: this.monitorRectLight.width * 3.4,
-              height: this.monitorRectLight.height * 3.4,
+              height: 0.28 * 3.4,
+              width: 0.71 * 3.4,
             },
             "same"
           )
           .to(
             this.rectAreaLight,
             {
-              width: this.rectAreaLight.width * 3.4,
-              height: this.rectAreaLight.height * 3.4,
+              height: 0.21 * 3.4,
+              width: 0.21 * 3.4,
             },
             "minifloor"
           )
@@ -528,11 +533,6 @@ export default class Controlls {
               ease: "back.out(2)",
               duration: 0.3,
             });
-            this.lampLight = GSAP.to(this.room.pointLight, {
-              power: 1,
-              ease: "back.out(2)",
-              duration: 0.3,
-            });
           }
           if (child.name === "FloorFirst") {
             this.fourth = GSAP.to(child.scale, {
@@ -591,7 +591,6 @@ export default class Controlls {
         this.secondPartTimeline.add(this.first);
         this.secondPartTimeline.add(this.second);
         this.secondPartTimeline.add(this.third);
-        this.secondPartTimeline.add(this.lampLight);
         this.secondPartTimeline.add(this.fourth, "-=0.2");
         this.secondPartTimeline.add(this.fifth, "-=0.2");
         this.secondPartTimeline.add(this.sixth, "-=0.2");
@@ -601,351 +600,6 @@ export default class Controlls {
       },
     });
   }
-  // setScrollTrigger() {
-  //   const scroll = GSAP.matchMedia();
-
-  //   //desktop
-  //   scroll.add("(min-width:969px)", () => {
-  //     console.log("desktop");
-  //     this.room.scale.set(0.11, 0.11, 0.11);
-  //     this.room.position.set(0, 0, 0);
-
-  //     // //monitor rectlight
-  //     this.room.children[21].height = 0.4;
-  //     this.room.children[21].width = 1;
-
-  //     // //aquariam rectlight
-  //     this.room.children[20].height = 0.7;
-  //     this.room.children[20].width = 0.5;
-
-  //     // //min floor lamp
-  //     this.room.children[8].children[2].height = 0.3;
-  //     this.room.children[8].children[2].width = 0.3;
-
-  //     this.room.children[8].children[3].power = 1;
-  //     //camera
-  //     this.camera.orthographicCamera.position.set(0, 6.5, 10);
-  //     //First section
-  //     this.firstMovetimeline = new GSAP.timeline();
-  //     this.firstMovetimeline.to(this.room.position, {
-  //       x: () => this.sizes.width * 0.0015,
-  //       scrollTrigger: {
-  //         trigger: ".first-margin",
-  //         start: "top top",
-  //         end: "bottom bottom",
-  //         scrub: 0.6,
-  //         invalidateOnRefresh: true,
-  //       },
-  //     });
-  //     //Second section
-  //     this.secondMovetimeline = new GSAP.timeline({
-  //       scrollTrigger: {
-  //         trigger: ".second-margin",
-  //         start: "top top",
-  //         end: "bottom bottom",
-  //         scrub: 0.6,
-  //         invalidateOnRefresh: true,
-  //       },
-  //     })
-  //       .to(
-  //         this.room.position,
-  //         {
-  //           x: () => 1,
-  //           z: () => this.sizes.height * 0.0032,
-  //         },
-  //         "same"
-  //       )
-  //       .to(
-  //         this.room.scale,
-  //         {
-  //           x: 0.4,
-  //           y: 0.4,
-  //           z: 0.4,
-  //         },
-  //         "same"
-  //       )
-  //       .to(
-  //         this.room.children[21],
-  //         {
-  //           height: this.room.children[21].height * 4,
-  //           width: this.room.children[21].width * 4,
-  //         },
-  //         "same"
-  //       )
-  //       .to(
-  //         this.room.children[20],
-  //         {
-  //           height: this.room.children[20].height * 4,
-  //           width: this.room.children[20].width * 4,
-  //         },
-  //         "same"
-  //       )
-  //       .to(
-  //         this.room.children[8].children[2],
-  //         {
-  //           height: 0.3 * 4,
-  //           width: 0.3 * 4,
-  //         },
-  //         "same"
-  //       )
-  //       .to(
-  //         this.room.children[8].children[3],
-  //         {
-  //           power: 1 * 4,
-  //         },
-  //         "same"
-  //       );
-
-  //     this.thirdMovetimeline = new GSAP.timeline({
-  //       scrollTrigger: {
-  //         trigger: ".third-margin",
-  //         start: "top top",
-  //         end: "bottom bottom",
-  //         scrub: 0.6,
-  //         invalidateOnRefresh: true,
-  //       },
-  //     }).to(this.camera.orthographicCamera.position, {
-  //       x: -4.1,
-  //       y: -1,
-  //     });
-  //   });
-  //   //mobile
-  //   scroll.add("(max-width:968px)", () => {
-  //     console.log("mobile");
-
-  //     //reset
-  //     this.room.scale.set(0.07, 0.07, 0.07);
-  //     this.room.position.set(0, 0, 0);
-
-  //     //monitor rectlight
-  //     this.room.children[21].height = this.room.children[21].height * 0.7;
-  //     this.room.children[21].width = this.room.children[21].width * 0.7;
-
-  //     //aquariam rectlight
-  //     this.room.children[20].height = this.room.children[20].height * 0.7;
-  //     this.room.children[20].width = this.room.children[20].width * 0.7;
-
-  //     //min floor lamp
-  //     this.room.children[8].children[2].height =
-  //       this.room.children[8].children[2].height * 0.7;
-  //     this.room.children[8].children[2].width =
-  //       this.room.children[8].children[2].width * 0.7;
-
-  //     this.room.children[8].children[3].power = 0.2;
-
-  //     //camera
-  //     this.camera.orthographicCamera.position.set(0, 6.5, 10);
-
-  //     // First section
-  //     this.firstMoveTimeline = new GSAP.timeline({
-  //       scrollTrigger: {
-  //         trigger: ".first-margin",
-  //         start: "top top",
-  //         end: "bottom bottom",
-  //         scrub: 0.6,
-  //         invalidateOnRefresh: true,
-  //       },
-  //     })
-  //       .to(
-  //         this.room.scale,
-  //         {
-  //           x: 0.1,
-  //           y: 0.1,
-  //           z: 0.1,
-  //         },
-  //         "same"
-  //       )
-  //       .to(
-  //         this.room.children[20],
-  //         {
-  //           width: this.room.children[20].width * 1.4,
-  //           height: this.room.children[20].height * 1.4,
-  //         },
-  //         "same"
-  //       )
-  //       .to(
-  //         this.room.children[21],
-  //         {
-  //           width: this.room.children[21].width * 1.4,
-  //           height: this.room.children[21].height * 1.4,
-  //         },
-  //         "same"
-  //       );
-
-  //     //second
-  //     this.secondMoveTimeline = new GSAP.timeline({
-  //       scrollTrigger: {
-  //         trigger: ".second-margin",
-  //         start: "top top",
-  //         end: "bottom bottom",
-  //         scrub: 0.6,
-  //         invalidateOnRefresh: true,
-  //       },
-  //     })
-  //       .to(
-  //         this.room.scale,
-  //         {
-  //           x: 0.25,
-  //           y: 0.25,
-  //           z: 0.25,
-  //         },
-  //         "same"
-  //       )
-  //       .to(
-  //         this.room.children[20],
-  //         {
-  //           width: this.room.children[20].width * 3.4,
-  //           height: this.room.children[20].height * 3.4,
-  //         },
-  //         "same"
-  //       )
-  //       .to(
-  //         this.room.children[21],
-  //         {
-  //           width: this.room.children[21].width * 3.4,
-  //           height: this.room.children[21].height * 3.4,
-  //         },
-  //         "same"
-  //       )
-  //       .to(
-  //         this.room.children[8].children[2],
-  //         {
-  //           width: this.room.children[8].children[2].width * 3.4,
-  //           height: this.room.children[8].children[2].height * 3.4,
-  //         },
-  //         "same"
-  //       )
-  //       .to(
-  //         this.room.children[8].children[3],
-  //         {
-  //           power: 1,
-  //         },
-  //         "same"
-  //       )
-  //       .to(
-  //         this.room.position,
-  //         {
-  //           x: 1.5,
-  //         },
-  //         "same"
-  //       );
-
-  //     //third
-  //     this.thirdMoveTimeline = new GSAP.timeline({
-  //       scrollTrigger: {
-  //         trigger: ".third-margin",
-  //         start: "top top",
-  //         end: "bottom bottom",
-  //         scrub: 0.6,
-  //         invalidateOnRefresh: true,
-  //       },
-  //     }).to(this.room.position, {
-  //       z: -4.5,
-  //     });
-  //   });
-
-  //   // scroll.add("(min-width:0px)", () => {
-  //   //   console.log("fire at all level");
-  //   //   // Mini Platform Animations
-  //   //   this.secondPartTimeline = new GSAP.timeline({
-  //   //     scrollTrigger: {
-  //   //       trigger: ".third-margin",
-  //   //       start: "center center",
-  //   //       markers: true,
-  //   //     },
-  //   //   });
-
-  //   //   this.room.children.forEach((child) => {
-  //   //     console.log(child);
-  //   //     if (child.name === "Mini_Floor") {
-  //   //       this.first = GSAP.to(child.position, {
-  //   //         x: -5.44055,
-  //   //         z: 13.6135,
-  //   //         duration: 0.3,
-  //   //       });
-  //   //     }
-  //   //     if (child.name === "Mailbox") {
-  //   //       this.second = GSAP.to(child.scale, {
-  //   //         x: 1,
-  //   //         y: 1,
-  //   //         z: 1,
-  //   //         duration: 0.3,
-  //   //       });
-  //   //     }
-  //   //     if (child.name === "Lamp") {
-  //   //       this.third = GSAP.to(child.scale, {
-  //   //         x: 1,
-  //   //         y: 1,
-  //   //         z: 1,
-  //   //         ease: "back.out(2)",
-  //   //         duration: 0.3,
-  //   //       });
-  //   //     }
-  //   //     if (child.name === "FloorFirst") {
-  //   //       this.fourth = GSAP.to(child.scale, {
-  //   //         x: 1,
-  //   //         y: 1,
-  //   //         z: 1,
-  //   //         ease: "back.out(2)",
-  //   //         duration: 0.3,
-  //   //       });
-  //   //     }
-  //   //     if (child.name === "FloorSecond") {
-  //   //       this.fifth = GSAP.to(child.scale, {
-  //   //         x: 1,
-  //   //         y: 1,
-  //   //         z: 1,
-  //   //         duration: 0.3,
-  //   //       });
-  //   //     }
-  //   //     if (child.name === "FloorThird") {
-  //   //       this.sixth = GSAP.to(child.scale, {
-  //   //         x: 1,
-  //   //         y: 1,
-  //   //         z: 1,
-  //   //         ease: "back.out(2)",
-  //   //         duration: 0.3,
-  //   //       });
-  //   //     }
-  //   //     if (child.name === "Dirt") {
-  //   //       this.seventh = GSAP.to(child.scale, {
-  //   //         x: 1,
-  //   //         y: 1,
-  //   //         z: 1,
-  //   //         ease: "back.out(2)",
-  //   //         duration: 0.3,
-  //   //       });
-  //   //     }
-  //   //     if (child.name === "Flower1") {
-  //   //       this.eighth = GSAP.to(child.scale, {
-  //   //         x: 1,
-  //   //         y: 1,
-  //   //         z: 1,
-  //   //         ease: "back.out(2)",
-  //   //         duration: 0.3,
-  //   //       });
-  //   //     }
-  //   //     if (child.name === "Flower2") {
-  //   //       this.ninth = GSAP.to(child.scale, {
-  //   //         x: 1,
-  //   //         y: 1,
-  //   //         z: 1,
-  //   //         ease: "back.out(2)",
-  //   //         duration: 0.3,
-  //   //       });
-  //   //     }
-  //   //   });
-  //   //   this.secondPartTimeline.add(this.first);
-  //   //   this.secondPartTimeline.add(this.second);
-  //   //   this.secondPartTimeline.add(this.third);
-  //   //   this.secondPartTimeline.add(this.fourth, "-=0.2");
-  //   //   this.secondPartTimeline.add(this.fifth, "-=0.2");
-  //   //   this.secondPartTimeline.add(this.sixth, "-=0.2");
-  //   //   this.secondPartTimeline.add(this.seventh, "-=0.2");
-  //   //   this.secondPartTimeline.add(this.eighth);
-  //   //   this.secondPartTimeline.add(this.ninth, "-=0.1");
-  //   // });
-  // }
 
   resize() {}
   update() {}
